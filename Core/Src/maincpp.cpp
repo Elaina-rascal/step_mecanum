3616eb3dfca5274bd3134d4fc17c1196a618f707
Elaina <1463967532@qq.com>
2025-04-04 19:31:30 +0800

    refactor将主函数从list 改成数组

--- Core/Src/maincpp.cpp
+++ Core/Src/maincpp.cpp
@@ -14,13 +14,12 @@ __asm(".global __use_no_semihosting");
 #include "FreeRTOS.h"
 #include "task.h"
 #include "stepmotorZDT.hpp"
-#include "Lib_List.h"
 // 实例化Map并将初始点设置成startInfo
 // Controller::Controller_t *ChassisControl;
 // Kinematic::Kinematic_t *kinematic;
 // Planner::Planner_t *planner;
 // StepMotorZDT_t *stepmotor_ptr;
-LibList_t<StepMotorZDT_t *> *stepmotor_list_ptr;
+StepMotorZDT_t *stepmotor_list_ptr[4];
 // TaskHandle_t Motor_control_handle;    // 电机转速控制
 // TaskHandle_t Kinematic_update_handle; // 运动学更新
 TaskHandle_t main_cpp_handle; // 主函数
@@ -40,11 +39,11 @@ void OnPlannerUpdate(void *pvParameters);
 void main_cpp(void)
 {
   // stepmotor_ptr = new StepMotorZDT_t(1, &huart1, true, 1);
-  stepmotor_list_ptr = new LibList_t<StepMotorZDT_t *>();
-  stepmotor_list_ptr->Add(new StepMotorZDT_t(1, &huart1, true, 1));
-  stepmotor_list_ptr->Add(new StepMotorZDT_t(2, &huart1, true, 1));
-  stepmotor_list_ptr->Add(new StepMotorZDT_t(3, &huart1, true, 1));
-  stepmotor_list_ptr->Add(new StepMotorZDT_t(4, &huart1, true, 1));
+  // stepmotor_list_ptr = new LibList_t<StepMotorZDT_t *>();
+  stepmotor_list_ptr[0] = new StepMotorZDT_t(1, &huart1, true, 1);
+  stepmotor_list_ptr[1] = new StepMotorZDT_t(2, &huart1, true, 1);
+  stepmotor_list_ptr[2] = new StepMotorZDT_t(3, &huart1, true, 1);
+  stepmotor_list_ptr[3] = new StepMotorZDT_t(4, &huart1, true, 1);
   //   HAL_UARTEx_ReceiveToIdle_DMA(&huart3, imu.buffer, 100);
   //   BaseType_t ok = xTaskCreate(OnMotorControl, "Motor_control", 600, NULL, 3, &Motor_control_handle);
   //   BaseType_t ok2 = xTaskCreate(OnKinematicUpdate, "Kinematic_update", 600, NULL, 2, &Kinematic_update_handle);
@@ -82,12 +81,13 @@ void ontest(void *pvParameters)
 {
   while (1)
   {
-    stepmotor_list_ptr->Foreach([](StepMotorZDT_t *motor)
-                                {
-      motor->set_speed_target(1.5);
+    for (int i = 0; i < 4; i++)
+    {
+      stepmotor_list_ptr[i]->set_speed_target(0.5);
       vTaskDelay(1000);
-      motor->set_speed_target(0.0);
-      vTaskDelay(1000); });
+      stepmotor_list_ptr[i]->set_speed_target(0.0);
+      vTaskDelay(1000);
+    }
   }
 }
 // void OnMotorControl(void *pvParameters)
